71069654315491157a2dc65880f62bef378cd0e0
Md Asmaul Hossain Sajib <90277851+Ssajib902@users.noreply.github.com>
2025-01-25 22:54:15 +0600

    Update useAxiosPublic.jsx

--- ecoprod-client/src/Hooks/useAxiosPublic.jsx
+++ ecoprod-client/src/Hooks/useAxiosPublic.jsx
@@ -2,7 +2,9 @@ import axios from 'axios'
 import React from 'react'
 
 const axiosPublic = axios.create( {
-    baseURL: 'http://localhost:5000'
+    
+    baseURL: 'https://ecoprod-backend.onrender.com'
+//    baseURL: 'http://localhost:5000'
 })
 
 const useAxiosPublic = () => {
